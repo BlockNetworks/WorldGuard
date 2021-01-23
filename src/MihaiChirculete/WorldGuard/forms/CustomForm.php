53521937620b407fb715089a89ac0d0d177c9373
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-01-22 21:44:13 -0800

    Update CustomForm.php

--- src/MihaiChirculete/WorldGuard/forms/CustomForm.php
+++ src/MihaiChirculete/WorldGuard/forms/CustomForm.php
@@ -1,66 +1,82 @@
 <?php
+
 declare(strict_types=1);
+
 namespace MihaiChirculete\WorldGuard\forms;
+
 use Closure;
 use MihaiChirculete\WorldGuard\elements\Element;
-use pocketmine\{form\FormValidationException, Player, utils\Utils};
+use pocketmine\form\FormValidationException;
+use pocketmine\utils\Utils;
+use pocketmine\Player;
 use function array_merge;
 use function gettype;
 use function is_array;
-class CustomForm extends Form{
+
+class CustomForm extends Form {
+
 	/** @var Element[] */
 	protected $elements;
 	/** @var Closure */
 	private $onSubmit;
 	/** @var Closure|null */
 	private $onClose;
+
 	/**
 	 * @param string       $title
 	 * @param Element[]    $elements
 	 * @param Closure      $onSubmit
 	 * @param Closure|null $onClose
 	 */
-	public function __construct(string $title, array $elements, Closure $onSubmit, ?Closure $onClose = null){
+	public function __construct(string $title, array $elements, Closure $onSubmit, ?Closure $onClose = null) {
 		parent::__construct($title);
 		$this->elements = $elements;
 		$this->onSubmit = $onSubmit;
 		$this->onClose = $onClose;
-		Utils::validateCallableSignature(function(Player $player, CustomFormResponse $response) : void{}, $onSubmit);
+		Utils::validateCallableSignature(function(Player $player, CustomFormResponse $response) : void {
+
+		}, $onSubmit);
 		$this->onSubmit = $onSubmit;
-		if($onClose !== null){
-			Utils::validateCallableSignature(function(Player $player) : void{}, $onClose);
+		if ($onClose !== null) {
+			Utils::validateCallableSignature(function(Player $player) : void {
+
+			}, $onClose);
 			$this->onClose = $onClose;
 		}
 	}
+
 	/**
 	 * @param Element ...$elements
 	 *
 	 * @return $this
 	 */
-	public function append(Element ...$elements) : self{
+	public function append(Element ...$elements) : self {
 		$this->elements = array_merge($this->elements, $elements);
 		return $this;
 	}
+
 	/**
 	 * @return string
 	 */
-	final public function getType() : string{
+	final public function getType() : string {
 		return self::TYPE_CUSTOM_FORM;
 	}
+
 	/**
 	 * @return array
 	 */
-	protected function serializeFormData() : array{
+	protected function serializeFormData() : array {
 		return ["content" => $this->elements];
 	}
-	final public function handleResponse(Player $player, $data) : void{
-		if($data === null){
-			if($this->onClose !== null){
+
+	final public function handleResponse(Player $player, $data) : void {
+		if ($data === null) {
+			if ($this->onClose !== null) {
 				($this->onClose)($player);
 			}
-		}elseif(is_array($data)){
-			foreach($data as $index => $value){
-				if(!isset($this->elements[$index])){
+		} elseif (is_array($data)) {
+			foreach ($data as $index => $value) {
+				if (!isset($this->elements[$index])) {
 					throw new FormValidationException("Element at index $index does not exist");
 				}
 				$element = $this->elements[$index];
@@ -68,8 +84,9 @@ class CustomForm extends Form{
 				$element->setValue($value);
 			}
 			($this->onSubmit)($player, new CustomFormResponse($this->elements));
-		}else{
+		} else {
 			throw new FormValidationException("Expected array or null, got " . gettype($data));
 		}
 	}
+
 }
