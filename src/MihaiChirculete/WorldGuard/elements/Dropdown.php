66c8b515c4451827a9dbc2458ba986b43a10e195
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-01-22 21:35:45 -0800

    Update Dropdown.php

--- src/MihaiChirculete/WorldGuard/elements/Dropdown.php
+++ src/MihaiChirculete/WorldGuard/elements/Dropdown.php
@@ -1,59 +1,72 @@
 <?php
+
 declare(strict_types=1);
+
 namespace MihaiChirculete\WorldGuard\elements;
+
 use pocketmine\form\FormValidationException;
-class Dropdown extends Element{
+
+class Dropdown extends Element {
+
 	/** @var string[] */
 	protected $options;
 	/** @var int */
 	protected $default;
+
 	/**
 	 * @param string   $text
 	 * @param string[] $options
 	 * @param int      $default
 	 */
-	public function __construct(string $text, array $options, int $default = 0){
+	public function __construct(string $text, array $options, int $default = 0) {
 		parent::__construct($text);
 		$this->options = $options;
 		$this->default = $default;
 	}
+
 	/**
 	 * @return array
 	 */
-	public function getOptions() : array{
+	public function getOptions() : array {
 		return $this->options;
 	}
+
 	/**
 	 * @return string
 	 */
-	public function getSelectedOption() : string{
+	public function getSelectedOption() : string {
 		return $this->options[$this->value];
 	}
+
 	/**
 	 * @return int
 	 */
-	public function getDefault() : int{
+	public function getDefault() : int {
 		return $this->default;
 	}
+
 	/**
 	 * @return string
 	 */
-	public function getType() : string{
+	public function getType() : string {
 		return "dropdown";
 	}
+
 	/**
 	 * @return array
 	 */
-	public function serializeElementData() : array{
+	public function serializeElementData() : array {
 		return [
 			"options" => $this->options,
-			"default" => $this->default
+			"default" => $this->default,
 		];
 	}
-	public function validate($value) : void{
+
+	public function validate($value) : void {
 		parent::validate($value);
-		if(!isset($this->options[$value])){
+		if (!isset($this->options[$value])) {
 			throw new FormValidationException("Option with index $value does not exist in dropdown");
 		}
 	}
+
 }
